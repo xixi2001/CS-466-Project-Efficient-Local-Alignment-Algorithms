cce7f2d0dac3748e16946ff69ae8e765b5d146f8
ZiyaoZhang_Maggie <zzymaggie@yahoo.com>
2022-12-17 13:41:22 -0600

    change file name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import hirschbergLocalAlignment, utility, linearSpaceCounting, tracebakAll
+import hirschbergLocalAlignment, utility, linearSpaceCounting, tracebackAll
 
 app = Flask(__name__)
 
@@ -17,6 +17,6 @@ def compute():
     local_alignment = hirschbergLocalAlignment.linear_space_local_align_solution(sequence1, sequence2, delta)
     best_score = utility.computeScore(local_alignment[0], local_alignment[1], delta)
     num_of_alignment = linearSpaceCounting.linear_space_local_align(sequence1, sequence2, delta)
-    tracebakAll.list.clear()
-    tracebakAll.local_align(sequence1, sequence2, delta)
-    return render_template('index.html', local_alignment=local_alignment, best_score=best_score, num_of_alignment=num_of_alignment, all_alignments=tracebakAll.list)
+    tracebackAll.list.clear()
+    tracebackAll.local_align(sequence1, sequence2, delta)
+    return render_template('index.html', local_alignment=local_alignment, best_score=best_score, num_of_alignment=num_of_alignment, all_alignments=tracebackAll.list)
